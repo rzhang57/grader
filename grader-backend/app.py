e0f1e7b5549f50a0863fa82624adbf88ec20ab3f
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2024-06-05 21:41:27 -0700

    Attempt at image processing 1
    Image processing works: image>numpy array>grayscale>binary (black/ white) through thresholding (127)>reecode (to return back to frontend)
    
    Thresholding needs work -> even with a scanned test image, raw thresholding doesn't seem to be the best at image clarity - look into adaptive thresholding
    
    Next steps: find contours, build string -> logic to grading

--- grader-backend/app.py
+++ grader-backend/app.py
@@ -1,7 +1,9 @@
 from flask import  Flask, request, jsonify, send_file
 from flask_cors import CORS
 import io
+import cv2
 from PIL import Image
+import numpy as np
 
 
 
@@ -16,9 +18,26 @@ def default():
 def analyzeImage():
     print("Received POST request to /analyze")
     
+    #image taken from frontend
     received_image = request.files['image']
     image_data = received_image.read()
-    image_stream = io.BytesIO(image_data)
-    return send_file(image_stream, mimetype='image/jpeg')
+    
+    #process/ decode image
+    numpy_converted = np.frombuffer(image_data, np.uint8)
+    analysis_base = cv2.imdecode(numpy_converted, cv2.IMREAD_GRAYSCALE)
+    tresh, binary_img = cv2.threshold(analysis_base, 127, 255, cv2.THRESH_BINARY) # converts to image to black/ white - might need to look into adaptive thresholding so lighting doesn't matter as much
+    success, reencoded_img = cv2.imencode('.jpg', binary_img)
+
+    if not success:
+        return "Encoding image failed", 500
+
+
+    #test
+    #test_raw_img = cv2.imread('test_sheet.jpg', cv2.IMREAD_GRAYSCALE)
+    #tresh, binary_img = cv2.threshold(test_raw_img, 127, 255, cv2.THRESH_BINARY)
+    #success, reencoded_img = cv2.imencode('.jpg', binary_img)
+
+
+    return send_file(io.BytesIO(reencoded_img), mimetype='image/jpeg')
     #return 'Success!'
 
