a925762c055aff4f544937f26d200f058466c36a
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2024-06-08 12:29:08 -0700

    Adaptive Thresholding implemented
    Might still need fine tuning with C value and different blocksize for averaging, but so far, seems to work fine.  Further tuning may be required for more accurate reading for OCR.

--- grader-backend/app.py
+++ grader-backend/app.py
@@ -17,27 +17,30 @@ def default():
 @app.route('/analyze', methods=['POST'])
 def analyzeImage():
     print("Received POST request to /analyze")
+
+    """
     
     #image taken from frontend
     received_image = request.files['image']
     image_data = received_image.read()
-    
     #process/ decode image
     numpy_converted = np.frombuffer(image_data, np.uint8)
     analysis_base = cv2.imdecode(numpy_converted, cv2.IMREAD_GRAYSCALE)
-    tresh, binary_img = cv2.threshold(analysis_base, 127, 255, cv2.THRESH_BINARY) # converts to image to black/ white - might need to look into adaptive thresholding so lighting doesn't matter as much
+    #tresh, binary_img = cv2.threshold(analysis_base, 127, 255, cv2.THRESH_BINARY) # converts to image to black/ white - might need to look into adaptive thresholding so lighting doesn't matter as much
+    binary_img = cv2.adaptiveThreshold(analysis_base, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 19, 4)
     success, reencoded_img = cv2.imencode('.jpg', binary_img)
 
     if not success:
         return "Encoding image failed", 500
+        """
 
 
     #test
-    #test_raw_img = cv2.imread('test_sheet.jpg', cv2.IMREAD_GRAYSCALE)
+    test_raw_img = cv2.imread('test_sheet.jpg', cv2.IMREAD_GRAYSCALE)
     #tresh, binary_img = cv2.threshold(test_raw_img, 127, 255, cv2.THRESH_BINARY)
-    #success, reencoded_img = cv2.imencode('.jpg', binary_img)
+    binary_img = cv2.adaptiveThreshold(test_raw_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 19, 4)
+    success, reencoded_img = cv2.imencode('.jpg', binary_img)
 
 
     return send_file(io.BytesIO(reencoded_img), mimetype='image/jpeg')
     #return 'Success!'
-
